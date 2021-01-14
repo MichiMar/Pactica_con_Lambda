d2e9676912cb86e2b175f5a31eb0aedd69a6e318
MichiMar <michimar.alexis@gmail.com>
2021-01-13 19:33:37 -0600

    funcin lambda creada

--- practica_mabda.py
+++ practica_mabda.py
@@ -1,8 +1,10 @@
-def area_triangulo(base, altura):
+'''def area_triangulo(base, altura):
     return (base*altura)/2
 
 triangulo1=area_triangulo(5,7)
 triangulo2=area_triangulo(9,6)
 
 print(triangulo1)
-print(triangulo2)
+print(triangulo2)'''
+
+area_triangulo=lambda base,altura: (base*altura)/2
